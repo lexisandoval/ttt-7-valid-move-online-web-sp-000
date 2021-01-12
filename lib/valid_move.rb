5381fc1eb310f75cc8ca11124315db9261b0290d
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-12 16:23:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,11 @@ def valid_move?(board, index)
   if (index > 8 || index < 0)
     return false
   else
-    position_taken?(board, index)
-    return true
+    if position_taken?(board, index)
+      return false
+    else
+      return true
+    end
   end
 end
 
